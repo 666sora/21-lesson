280165c38d897f7d1040e4dd5737697ae4585aa8
666sora <pavelbys03@gmail.com>
2022-05-10 22:05:04 +0700

    1-task done

---
+++ 1-task.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+struct statement {
+    std::string name = "unknown";
+    std::string surname = "unknown";
+    std::string date = "dd.mm.yyyy";
+    int amount = 0;
+};
+
+void add(std::ofstream& file, statement& payment) {
+    file << std::endl << payment.name << " " << payment.surname << " " << payment.date << " " << payment.amount;
+}
+
+void read(std::ifstream& file, statement& payment) {
+    file >> payment.name >> payment.surname >> payment.date >> payment.amount;
+    std::cout << payment.name << " " << payment.surname << " " << payment.date << " " << payment.amount << std::endl;
+}
+
+int main() {
+    statement payment;
+    std::string answer = "0";
+    while (answer != "-1") {
+        std::cout << "Choose which command you want to use: ";
+        std::cin >> answer;
+        if (answer == "add") {
+            std::cout << "Input name: ";
+            std::cin >> payment.name;
+            std::cout << "Input surname: ";
+            std::cin >> payment.surname;
+            std::cout << "Input date: ";
+            std::cin >> payment.date;
+            std::cout << "Input amount of payment: ";
+            std::cin >> payment.amount;
+            std::ofstream file("statement.txt", std::ios::app);
+            add(file, payment);
+            file.close();
+        }
+        else if (answer == "read") {
+            std::ifstream file("statement.txt");
+            while (!file.eof()) {
+                read(file, payment);
+            }
+        }
+    }
+}
+
+/*
+Вы уже создавали программу чтения и записи ведомости. Теперь её надо будет обобщить и дополнить использованием структур.
+Формат ведомости прежний. Сначала идёт имя и фамилия получателя денежных средств, далее — дата выдачи в формате ДД.ММ.ГГГГ.,
+затем — сумма выплаты в рублях. Данные разделяются между собой пробелами. В конце каждой записи должен быть перевод строки.
+Структура данных записи должна соответствовать указанному формату.
+При старте программы пользователь указывает команду, которую следует осуществить:
+list или add. Команда read осуществляет чтение из файла ведомости как и прежде, только уже в структуры данных и отображает их на экране.
+Команда add добавляет новую запись в конец ведомости.
+*/
